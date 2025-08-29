ad08cb1ddba46c39b5ac169c9d2a410e0810f276
Rapenzol <sampadajadhav26123@gmail.com>
2025-08-29 12:43:16 +0530

    index added to TaskSchema

--- models/Task.js
+++ models/Task.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  status: { type: String, enum: ["To Do", "In Progress", "Done"], default: "To Do" },
-  priority: { type: String, enum: ["Low", "Medium", "High"], default: "Medium" }
-}, { timestamps: true });
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    status: {
+      type: String,
+      enum: ["To Do", "In Progress", "Done"],
+      default: "To Do",
+      index: true,
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+      index: true,
+    }
+  },
+  { timestamps: true }
+);
+
+taskSchema.index({ status: 1, priority: 1 });
 
 module.exports = mongoose.model("Task", taskSchema);
